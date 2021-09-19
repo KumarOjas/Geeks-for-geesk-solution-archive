9c5119678cc8c0b209ff9b951b78d8f48b3de435
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-09-19 15:34:23 +0530

    Create check_for_bst.cpp

---
+++ check_for_bst.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Noode 
+{
+   int key;
+  struct Node *left;
+  struct Node *right;
+  Node (int k)
+  {
+  key=k;
+      left=right=NULL;
+  }
+};
+ 
+int prev=Int_Min;
+bool isBST(Node* root)  
+{  
+    if (root == NULL)  
+        return true;  
+    
+    if(isBST(root->left)==false)return false;
+    
+    if(root->key<=prevv)return false;
+    prevv=root->key;
+    
+    return isBST(root->right);
+}
+
+int main() {
+	
+	Node *root = new Node(4);  
+    root->left = new Node(2);  
+    root->right = new Node(5);  
+    root->left->left = new Node(1);  
+    root->left->right = new Node(3);  
+      
+    if(isBST(root))  
+        cout<<"Is BST";  
+    else
+        cout<<"Not a BST";  
+          
+    return 0;  
+	
+}
+
+
+  
+
+
+
+
+
+
+
+
+
+}
