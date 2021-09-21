de8b3fe3108212eb462ce222c11411cfd5135a8a
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-09-21 10:47:55 +0530

    Create 04)stack_using_2_queue_costly_push_approach.cpp

---
+++ 04)stack_using_2_queue_costly_push_approach.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Stack {
+
+     int n;
+  queue<int>q1;
+  queue<int>q2;
+  
+  public:
+  
+  stack ()
+  {
+   n=0;
+  }
+
+  
+  void push(int x)
+  {
+  q2.push(x);
+    while(!q1.empty());
+    while(!q1.empty())
+      
+    {
+       q2.push(q1.front());
+       q1.pop();
+     queue<int> temp =q1;
+      q1=q2;
+      q2=  temp;
+     n++;
+    }
+    
+		void pop()
+			{
+				if(q1.empty())
+					{
+						cout<<"Empty Stack";
+						return;
+					}
+				q1.pop();
+				n--;
+			}
+
+		int top(){	return q1.front();}
+		
+		int size(){return n;}
+
+	};
+
+int main()
+{
+	stackk s;
+	int n,x;
+	cin>>n;
+
+	for(int i=0;i<n;i++)
+		{
+			cin>>x;
+			s.push(x);
+		}
+
+	while(s.size()>0)
+		{
+			cout<<s.top()<<" ";
+			s.pop();
+		}
+
+	return 0;
+}
+    
+    
+    
+    }
+  
+  
+  
+  
+  } 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
