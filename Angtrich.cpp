2fd0b533d14772f499bde0a30884bd5dd05b26df
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-21 11:02:50 +0530

    Create Angtrich.cpp

---
+++ Angtrich.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  int a,b,c;
+
+  cin>>a>>b>>c;
+  
+  if(a!=0 && b!=0 && c!=0 && a+b+c ==180){
+  
+    cout<< "Yes";
+  }
+  else
+  {
+    cout<<"No";
+  }
+  return 0;
+}
+    
+    
+    
+    
+    
+    
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
