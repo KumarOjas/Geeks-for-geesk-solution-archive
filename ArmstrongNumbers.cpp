37ab44630881958841d54534d8ae39d178ef0ebd
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-21 11:09:50 +0530

    Create ArmstrongNumbers.cpp

---
+++ ArmstrongNumbers.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution 
+{
+
+
+
+
+
+public:
+    string armstrongNumber(int n)
+    {
+        int temp = n, sum = 0;
+        while (n != 0)
+        {
+            int r = n % 10;
+            sum = sum + r * r * r;
+            n /= 10;
+        }
+        if (temp == sum)
+        {
+            return "Yes";
+        }
+        else
+            return "No";
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
