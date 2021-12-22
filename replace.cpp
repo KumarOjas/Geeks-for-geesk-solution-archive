cb1ab6149c93b002d730ab7097b9a7223c8c2586
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-22 15:37:35 +0530

    Create replace.cpp

---
+++ replace.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  
+  public:
+  
+  vector<int> replaceElements(vector<int>& arr) {
+  
+    int len = arr.size();
+    
+    int biggest;
+    
+    for(int i =0; i < len-1; i++)
+    
+    {
+            biggest = INT_MIN; 
+        
+      for (int j= i+1; j < len; j++)
+      
+      {
+      
+        if(biggest < arr[j])
+        
+          biggest = arr[j];
+          
+      }
+            arr[i] = biggest;
+        }
+        
+        arr[len-1] = -1;
+        return arr; 
+    }
+};
