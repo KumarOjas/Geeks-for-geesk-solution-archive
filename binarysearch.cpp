ab379d0363bd36eea80f00a7abb9905d3ec69b2b
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-22 15:47:08 +0530

    Create binarysearch.cpp

---
+++ binarysearch.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+  
+   int search(vector<int>& nums, int target) {
+  
+     int left = 0;
+     
+     int right = nums.size()-1;
+        
+     
+     while (left <= right)
+     
+     {
+     
+       int middle = left + (right-left)/2;
+       
+       if(nums[middle] == target)
+       
+         return middle;
+         
+       if(nums[middle] > target)
+       
+         right--;
+         
+       else
+                left++;
+        }
+        return -1;
+    }
+};
