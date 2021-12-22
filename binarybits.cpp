f794ef9aa123c91a3db41bb83f13e30e0a344e3c
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-22 15:43:44 +0530

    Create binarybits.cpp

---
+++ binarybits.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  
+  public:
+  
+    bool hasAlternatingBits(int n){
+  
+      vector<bool>s;
+      
+      while(n > 0)
+      
+      {
+      
+        s.push_back(n%2);
+        
+        n /=2;
+        }
+        
+      for(int i =0; i < s.size()-1; i++)
+      
+      {
+      
+        if(s[i] == s[i+1])
+        
+          return false;
+        }
+       
+      return true;
+    }
+};
