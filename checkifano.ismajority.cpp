12681786bb3eb75d3284d937e634a59dd8c65f73
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-22 15:59:07 +0530

    Create checkifano.ismajority.cpp

---
+++ checkifano.ismajority.cpp
@@ -0,0 +1,24 @@
+class Solution {
+
+  public:
+    
+  bool isMajorityElement(vector<int>& nums, int target) {
+  
+      int len = nums.size(); 
+        
+        int counter = 0; 
+  
+    for(int i =0; i < len; i++)
+    
+    {
+    
+      if(nums[i] == target)
+      
+        counter++; 
+        
+    }
+        if(counter > len/2)
+            return true; 
+        return false; 
+    }
+};
