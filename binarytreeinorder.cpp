a23ee13a151619f11c532e99672dd2224be833a4
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-22 15:48:26 +0530

    Create binarytreeinorder.cpp

---
+++ binarytreeinorder.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  
+  public :
+  
+  vector<int>s;
+  
+  void inorder(TreeNode * root)
+  
+  {
+  
+    if(root == NULL)
+    
+      return; 
+      
+    inorder(root->left);
+    
+    s.push_back(root->val);
+    
+    inorder(root->right);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+    
+      inorder(root);
+      
+      return s;  // [1,2,3]
+    }
+};
