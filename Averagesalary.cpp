844399bdd2dd1b4e8d8ee73da729f4df039b794a
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-22 15:41:09 +0530

    Create Averagesalary.cpp

---
+++ Averagesalary.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  
+  public:
+  
+  double average(vector<int>& salary) {
+  
+    int len = salary.size();
+    
+    sort(salary.begin(), salary.end());
+    
+    int sum = 0; 
+        
+    for(int i =1; i < len-1; i++)
+    
+          sum += salary[i];
+    
+    return (double)sum/(len-2);
+    }
+};
