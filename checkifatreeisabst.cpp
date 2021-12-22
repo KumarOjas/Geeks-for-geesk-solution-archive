80839142602b125b9643a059a541540f585aba71
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-22 16:00:19 +0530

    Create checkifatreeisabst.cpp

---
+++ checkifatreeisabst.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  
+  public:
+   bool IsBSTHelper(TreeNode* node, int smallest, int biggest)
+ 
+   {
+   
+     if(node == NULL)
+     
+       return true; 
+    
+        int data = node->val;
+        if(smallest != -1 && data <= smallest) return false; 
+        if(biggest != -1 && data >= biggest) return false; 
+
+        if(! IsBSTHelper(node->right, data, biggest)) return false;
+
+        if (! IsBSTHelper(node->left, smallest, data)) return false;         
+        return true;
+    }
+    
+     bool isValidBST(TreeNode* root) {
+        return IsBSTHelper(root, -1,-1);
+   }
+};
