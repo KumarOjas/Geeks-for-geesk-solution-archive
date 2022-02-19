722eb133573831b5dc09724cc601195dc3275252
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-02-19 15:06:19 +0530

    Create Searchinarow-column sorted Matrix.cpp

---
+++ Searchinarow-column sorted Matrix.cpp
@@ -0,0 +1,41 @@
+
+int search(int n,int m,int x,int mat[SIZE][Size])
+
+{
+ 
+  int row=0,col=m-1;
+  
+  
+  while(row<n && col>=0 )
+  
+  
+  {
+  
+    
+    if(mat[row][col]<x)
+      
+    {  
+      row++;
+     
+    }
+    
+    else if(mat[row][col]>x)
+    
+    {
+    
+      col--;
+      
+    }
+       else if(mat[row][col]==x)
+    
+       {
+       
+         return 1;
+       }
+    
+   }
+   return 0;
+   
+}
+  
+  }
